c3b227c5f781a097f2194c074b039c90e19fd507
Timperator2 <Tim.Menzner@hs-coburg.de>
2023-12-05 16:21:21 +0100

    modified website content and added support for german language

---
+++ script.js
@@ -0,0 +1,129 @@
+const translations = {
+  "nav.about": {
+    "en": "About",
+    "de": "Über"
+  },
+  "nav.team": {
+    "en": "Team",
+    "de": "Team"
+  },
+  "nav.download": {
+    "en": "Download",
+    "de": "Download"
+  },
+  "nav.contact": {
+    "en": "Contact",
+    "de": "Kontakt"
+  },
+  "about.title": {
+    "en": "About Us",
+    "de": "Über uns"
+  },
+  "about.content": {
+    "en": "Welcome to the Bias Scanner Project! We're all about using technology to tackle bias in online news. Bias Scanner is here to help you navigate the sometimes tricky world of online news, so you can make more informed choices about what you read.",
+    "de": "Willkommen beim Bias Scanner Project! Wir setzen Technologie ein, um Voreingenommenheit in Online-Nachrichten zu bekämpfen. Der Bias Scanner soll Ihnen helfen, sich in der manchmal kniffligen Welt der Online-Nachrichten zurechtzufinden, damit Sie informiertere Entscheidungen darüber treffen können, was Sie lesen."
+  },
+  "whyscanner.title": {
+    "en": "Why BiasScanner?",
+    "de": "Warum BiasScanner?"
+  },
+  "whyscanner.content": {
+    "en": "In today's world, many of us get our news from the internet. Unfortunately, the web is also home to a lot of biased reporting, fake news, and other not-so-great stuff. Bias Scanner was created to give you a hand in spotting bias when you're reading news articles online.",
+    "de": "In der heutigen Welt beziehen viele von uns ihre Nachrichten aus dem Internet. Leider gibt es im Netz auch viele tendenziöse Berichte, Fake News und andere weniger gute Dinge. Der Bias Scanner wurde entwickelt, um Ihnen beim Erkennen von Voreingenommenheit zu helfen, wenn Sie Online-Nachrichten lesen."
+  },
+  "whatscanner.title": {
+    "en": "What's BiasScanner All About?",
+    "de": "Worum geht es bei BiasScanner?"
+  },
+  "whatscanner.content": {
+    "en": "BiasScanner is a handy browser plug-in that uses some fancy machine learning tech to find sentences in news articles that might be biased. It works right in your web browser, highlighting potentially biased sentences as you read. If you want to dive deeper, it can even give you a more detailed analysis of the article. The main point is not necessarily to ultimately decide what is biased and what is not. Rather, we want to raise awareness of the issue and encourage discussions and reflections.",
+    "de": "BiasScanner ist ein praktisches Browser-Plug-In, das einige ausgeklügelte Machine-Learning-Technologien verwendet, um Sätze in Nachrichtenartikeln zu finden, die möglicherweise voreingenommen sind. Es funktioniert direkt in Ihrem Webbrowser und markiert potenziell voreingenommene Sätze beim Lesen. Wenn Sie tiefer gehen möchten, kann es Ihnen sogar eine detailliertere Analyse des Artikels geben. Es geht dabei nicht unbedingt darum, in letzter Instanz zu entscheiden was jetzt voreingenommen ist und was nicht. Vielmehr möchten wir das Bewusstsein für das Thema schärfen und zu Diskussionen und Reflexionen anregen."
+  },
+  "whycool.title": {
+    "en": "Why It's Cool:",
+    "de": "Warum es cool ist:"
+  },
+  "whycool.inline": {
+    "en": "Inline Highlighting: It helps you spot potential bias as you read.",
+    "de": "Inline-Markierung: Es hilft Ihnen, potenzielle Voreingenommenheit beim Lesen zu erkennen."
+  },
+  "whycool.detailed": {
+    "en": "Detailed Analysis: If you're curious, it can provide a more in-depth look at the article.",
+    "de": "Detaillierte Analyse: Wenn Sie neugierig sind, kann es Ihnen eine eingehendere Betrachtung des Artikels ermöglichen."
+  },
+  "whycool.strength": {
+    "en": "Bias Strength: It rates how strong the detected bias is, from 0 to 1.",
+    "de": "Bias-Stärke: Es bewertet, wie stark die erkannte Voreingenommenheit ist, von 0 bis 1."
+  },
+  "whycool.multilingual": {
+    "en": "Multilingual Magic: BiasScanner not only speaks fluent English and German but also understands a plethora of other languages when it comes to dissecting news articles.",
+    "de": "Multilinguale Magie: BiasScanner spricht nicht nur fließend Englisch und Deutsch, sondern versteht auch eine Vielzahl anderer Sprachen, wenn es darum geht, Nachrichtenartikel zu zerlegen."
+  },
+  "whycool.privacy": {
+    "en": "Privacy Matters: We're big on privacy - no storing your personal info, ever. We only keep (anonymized) information about the news stories you read when you explicitly donate them for our research.",
+    "de": "Datenschutz ist wichtig: Wir legen großen Wert auf Datenschutz - Ihre persönlichen Informationen werden niemals gespeichert. Wir speichern nur (anonymisierte) Informationen über die von Ihnen gelesenen Artikel, die Sie ausdrücklich für unsere Forschung spenden."
+  },
+  "join.title": {
+    "en": "Join Us in the Fight Against Bias",
+    "de": "Schließen Sie sich uns im Kampf gegen Voreingenommenheit an"
+  },
+  "join.content": {
+    "en": "We believe technology can make news consumption better and protect democracy. So, why not give BiasScanner a try? By using it (and maybe even sharing interesting results with us trough the application), you not only help yourself, but you also contribute to ongoing research on bias in news. Let's work together to make online news a more reliable source of information.",
+    "de": "Wir glauben, dass Technologie die Nachrichtenkonsum verbessern und die Demokratie schützen kann. Also, warum nicht BiasScanner ausprobieren? Indem Sie es verwenden (und vielleicht sogar interessante Ergebnisse mit uns über die Anwendung teilen), helfen Sie nicht nur sich selbst, sondern tragen auch zur laufenden Forschung über Voreingenommenheit in den Nachrichten bei. Lassen Sie uns zusammenarbeiten, um Online-Nachrichten zu einer zuverlässigeren Informationsquelle zu machen."
+  },
+  "team.title": {
+    "en": "Our Team",
+    "de": "Unser Team"
+  },
+  "team.content": {
+    "en": "(Team info is hidden right now to ensure anonymity during a paper review process).",
+    "de": "(Team-Informationen sind derzeit ausgeblendet, um Anonymität während des Begutachtungsprozesses sicherzustellen)."
+  },
+  "download.title": {
+    "en": "Download",
+    "de": "Download"
+  },
+  "contact.title": {
+    "en": "Contact Us",
+    "de": "Kontaktieren Sie uns"
+  },
+  "contact.content": {
+    "en": "If you have any questions or would like to get involved, feel free to contact us at (Mail addresses are hidden right now to ensure anonymity during a paper review process).",
+    "de": "Wenn Sie Fragen haben oder sich beteiligen möchten, kontaktieren Sie uns gerne unter (E-Mail-Adressen sind derzeit ausgeblendet, um Anonymität während des Begutachtungsprozesses sicherzustellen)."
+  }
+};
+
+// Function to update the content based on the selected language
+function updateLanguage(selectedLanguage) {
+  document.querySelectorAll('[data-translate]').forEach(element => {
+    const translationKey = element.getAttribute('data-translate');
+    const translatedContent = translations[translationKey]?.[selectedLanguage];
+
+    if (translatedContent) {
+      element.textContent = translatedContent;
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const languageSelect = document.getElementById("language");
+
+  // Initial update of content based on the default language
+  const defaultLanguage = languageSelect.value; // Assuming 'en' is the default language
+  updateLanguage(defaultLanguage);
+
+  // Add an event listener for the 'change' event
+  languageSelect.addEventListener('change', () => {
+    // Get the selected value
+    const selectedLanguage = languageSelect.value;
+
+    // Update the content based on the selected language
+    updateLanguage(selectedLanguage);
+
+    // You can perform additional actions based on the selected language here
+    console.log('Selected language:', selectedLanguage);
+    // For example, you can redirect to a page based on the selected language
+    // window.location.href = `/${selectedLanguage}/index.html`;
+  });
+});
+
